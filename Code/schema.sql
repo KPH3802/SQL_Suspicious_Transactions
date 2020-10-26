f3a7adfac98a2af00d0fb48b3c7cd49a5ae0e56e
KPH3802 <kph3802@gmail.com>
2020-10-26 15:29:02 -0500

    Updated schema with foreign keys

--- Code/schema.sql
+++ Code/schema.sql
@@ -1,15 +1,18 @@
 DROP TABLE IF EXISTS card_holder;
 
 CREATE TABLE card_holder(
-	id INT PRIMARY KEY NOT NULL,
-	name VARCHAR(255)
+	id INT NOT NULL,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS credit_card;
 
 CREATE TABLE credit_card(
-	card VARCHAR(20) PRIMARY KEY,
-	id_card_holder INT
+	card VARCHAR(20),
+	id_card_holder INT,
+	PRIMARY KEY (card),
+	FOREIGN KEY id_card_holder REFERENCES card_holder(id)
 );
 
 DROP TABLE IF EXISTS transaction;
@@ -19,21 +22,27 @@ CREATE TABLE transaction(
 	date timestamp,
 	amount FLOAT,
 	card VARCHAR(20),
-	id_merchant INT
+	id_merchant INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY card REFERENCES credit_card(card),
+	FOREIGN KEY id_merchant REFERENCES merchant(id)
 );
 
 DROP TABLE IF EXISTS merchant_category;
 
 CREATE TABLE merchant_category(
-	Cardid INT PRIMARY KEY,
-	name VARCHAR(255)
+	Cardid INT,
+	name VARCHAR(255),
+	PRIMARY KEY (Cardid)
 );
 
 DROP TABLE IF EXISTS merchant;
 
 CREATE TABLE merchant(
-	id INT PRIMARY KEY,
+	id INT,
 	name VARCHAR(255),
-	id_merchant_category INT
+	id_merchant_category INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY id_merchant_category REFERENCES merchant_category(Cardid)
 );
 
